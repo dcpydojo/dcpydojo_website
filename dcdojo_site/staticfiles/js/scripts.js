9dd9297c1e13e62b9fd76ba453bc5c0fed4f39e1
Nathan Danielsen <nathan.danielsen@gmail.com>
2016-03-01 13:39:09 -0500

    added static files probably too much

---
+++ dcdojo_site/staticfiles/js/scripts.js
@@ -0,0 +1,44 @@
+
+
+  // A $( document ).ready() block.
+$( document ).ready(function() {
+      $(".button-collapse").sideNav();
+
+
+
+        $('input#ajax_form').on('click', function() { 
+            console.log("form submitted!"); 
+
+            var name = $('input[name="email"]').val();
+            var csrf_token = $("input[name='csrfmiddlewaretoken']").val()
+
+
+            var post_data = {        'name': name, 
+
+                      'csrfmiddlewaretoken': csrf_token,
+                           };
+
+            console.log( post_data );
+            event.preventDefault();
+            create_post(post_data);
+            return false;
+
+        });
+
+            function create_post( post_data ) {
+            console.log("create post is working!") // sanity check
+            $.ajax({
+                url : "slack/form/", // the endpoint
+                type : "POST", // http method
+                traditional: true,
+                data : post_data , // data sent with the post request
+
+                // handle a successful response
+                success : function(data) { 
+                    console.log("success"); // another sanity check
+
+                }
+            });
+        }
+
+});
